857a4bcec0227eddde2069de3e0d9600ab083182
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-01-16 16:33:34 +0100

    style: aggiunto commenti in header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,12 +5,14 @@ export default {
 </script>
 
 <template>
+  <!-- nav di ricerca per utente -->
   <div class="barra-ricerca">
     <nav class="navbar navbar-expand-lg navbar-light bg-light bg-transparent">
       <div class="container-fluid">
         <a class="navbar-brand" href="#"
           ><img src="../assets/logo.png" alt=""
         /></a>
+        <!-- bottone per fare drop down delle pagine disponibili -->
         <button
           class="navbar-toggler"
           type="button"
@@ -26,6 +28,7 @@ export default {
           class="collapse navbar-collapse justify-content-end py-2"
           id="navbarNavDropdown"
         >
+          <!-- ul per cambiare pagine  -->
           <ul class="navbar-nav p-2">
             <li class="nav-item my-1 ps-2">
               <a class="nav-link active" aria-current="page" href="#">
@@ -43,7 +46,7 @@ export default {
               </a>
             </li>
           </ul>
-          <span id="citazione" class="">
+          <span id="citazione">
             <a href="#"> Get a Quote </a>
           </span>
         </div>
@@ -55,17 +58,19 @@ export default {
 <style lang="scss" scoped>
 @use "../style/partials/variables" as *;
 @use "../style/partials/mixins" as *;
-
+// caratteristiche contenitore dove c'è nav bar
 .barra-ricerca {
   width: 100%;
   position: fixed;
   z-index: 100;
+  // caratteristiche navbar
   nav {
     @include contain;
 
     a {
       color: $white;
     }
+    // caratteristiche bottone citazioni
     #citazione {
       display: inline-block;
       a {
@@ -75,17 +80,19 @@ export default {
         border: 1px solid white;
       }
     }
+    // caratteristiche hover
     #citazione:hover a {
       background-color: #260458;
       color: white;
     }
   }
+  // caratteristiche hover li dentro alla lista delle pagine 
+  li a:hover {
+    color: $orange;
+  }
 }
 
-li a:hover {
-  color: $orange;
-}
-
+// caratteristiche al cambiare della larghezza shermo 
 @media screen and (max-width: 991px) {
   #navbarNavDropdown {
     background-color: #260458;

24142d047882eed4f4955fa5183970e159cd2cf2
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-01-16 12:07:25 +0100

    fix: sistemato la nav bar aggiungendo position fixed

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,78 +5,65 @@ export default {
 </script>
 
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNavDropdown"
-        aria-controls="navbarNavDropdown"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">
-              <router-link :to="{ name: 'about' }"> about </router-link>
-            </a>
-          </li>
-          <li class="nav-item">
-            <!-- <a class="nav-link" href="#"> -->
-            <a class="nav-link active" aria-current="page" href="#">
-              <router-link :to="{ name: 'home' }"> home </router-link>
-            </a>
-            <!-- </a> -->
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <router-link :to="{ name: 'contacts' }"> contacts </router-link>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-
-  <!-- <nav class="navbar navbar-expand-sm navbar-light bg-light p-1 ">
-        <a class="navbar-brand text-danger" href="#"><img src="../assets/logo.png" alt=""></a>
-        <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-            <form >
-              <ul class="d-flex ">
-                <li class="mx-1">
-                  <a class="nav-link active" aria-current="page" href="#">
-                     <router-link :to="{ name: 'home' }">
-                        home
-                      </router-link>
-                    </a>
-                </li>
-                <li class="mx-1">
-                  <a class="nav-link" href="#">
-                    <router-link :to="{ name: 'about' }">
-                     about
-                    </router-link>
-                   </a>
-                </li>
-                <li class="mx-1">
-                  <a class="nav-link " href="#" >
-                     <router-link :to="{ name: 'contacts' }">
-                    contacts
-                    </router-link>
-                   </a>                   
-                </li>
-              </ul>
-              
-            </form>
+  <div class="barra-ricerca">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light bg-transparent">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#"
+          ><img src="../assets/logo.png" alt=""
+        /></a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div
+          class="collapse navbar-collapse justify-content-end"
+          id="navbarNavDropdown"
+        >
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">
+                <router-link :to="{ name: 'about' }"> about </router-link>
+              </a>
+            </li>
+            <li class="nav-item">
+              <!-- <a class="nav-link" href="#"> -->
+              <a class="nav-link active" aria-current="page" href="#">
+                <router-link :to="{ name: 'home' }"> home </router-link>
+              </a>
+              <!-- </a> -->
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <router-link :to="{ name: 'contacts' }"> contacts </router-link>
+              </a>
+            </li>
+          </ul>
         </div>
-    </nav> -->
+      </div>
+    </nav>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 @use "../style/partials/variables" as *;
 @use "../style/partials/mixins" as *;
+
+.barra-ricerca {
+  width: 100%;
+  position: fixed;
+  nav {
+    @include contain;
+
+    a {
+      color: black;
+    }
+  }
+}
 </style>
